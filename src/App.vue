5d12de3a05c796f544ffea277abc95794706033e
fabuzard <44804900+fabuzard@users.noreply.github.com>
2024-03-19 14:51:04 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+import SearchInput from './components/SearchInput.vue';
+import WeatherCard from './components/WeatherCard.vue';
+const places = ref([])
+const addPlace=(data)=>{
+  places.value.push(data)
+  console.log(data)
+}
+
+const deletePlace=(name)=>{
+  places.value=places.value.filter(p=>p.location.name!==name)
+}
+</script>
+<template>
+  <main>
+    <!-- Date -->
+    <div class="text-center mb-6">
+      {{ new Date().toLocaleDateString('en-us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      }) }}
+    </div>
+<!-- Search -->
+    <div>
+      <SearchInput @place-data="addPlace" />
+
+    </div>
+<!-- Weather Card -->
+<div class="grid grid-cols-2 gap-4">
+  <div v-for="(place, idx) in places" :key="idx">
+        <WeatherCard :place="place" @delete-place="deletePlace" />
+      </div>
+</div>
+  </main>
+
+</template>
